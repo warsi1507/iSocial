8dedd3a02be9fd48e0d037d4e9c904e6277e9c74
Samar <mdsamarwarsi@outlook.com>
2024-12-27 16:06:32 +0530

    set up assets and dynamic layout

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const expressLayout = require('express-ejs-layouts')
+
 
 const app = express();
 const port = 8000;
 
+// use static files 
+app.use(expressLayout)
+
+// extract styles and scripts from sub pages into the layouts
+app.set('layout extractStyles', true)
+app.set('layout extractScripts', true)
+
+// use express router
 app.use('/', require('./routes/index.js'));
 
+// set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
