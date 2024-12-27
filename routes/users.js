52f52bae378a1724c39ee7ef3115078f2ce2bdbe
Samar <mdsamarwarsi@outlook.com>
2024-12-27 12:05:36 +0530

    add postcontroller and route

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const usersController = require('../controllers/users_controller.js');
-
+const postsController = require('../controllers/post_controller.js');
 
 router.get('/profile',usersController.profile);
+router.get('/posts',postsController.posts);
 
 module.exports = router;
