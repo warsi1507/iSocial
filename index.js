574c0f1561a78c2f4ea2ec5f7fd2020f138c2af2
Samar <mdsamarwarsi@outlook.com>
2024-12-27 10:28:56 +0530

    create the directory structure and setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+
+const app = express();
+const port = 8000;
+
+
+app.listen(port, (err)=>{
+    if(err){
+        console.error(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+})
