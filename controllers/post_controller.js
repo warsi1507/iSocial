88e05440a78560b3e5802a127b00c8369d167961
Samar <mdsamarwarsi@outlook.com>
2025-01-09 10:22:53 +0530

    create delete post controller, route and action

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
-const Post = require('../models/post')
+const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = async function(req, res){
     try {
@@ -12,6 +13,26 @@ module.exports.create = async function(req, res){
         return res.redirect(req.get('Referer') || '/');
     } catch (err) {
         console.error("Error in post creation:", err);
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+module.exports.destroy = async function (req, res) {
+    try {
+        let post = await Post.findById(req.params.id);
+
+        // .id is string(._id)
+        if (post.user == req.user.id) {
+            await post.deleteOne();
+
+            await Comment.deleteMany({ post: req.params.id });
+
+            return res.redirect(req.get('Referer') || '/');
+        } else {
+            return res.redirect('/');
+        }
+    } catch (err) {
+        console.error("Error in post deletion:", err);
         return res.status(500).send("Internal Server Error");
     }
 }
