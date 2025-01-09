64224f84216caadf1f479b499ec10f24a62146a1
Samar <mdsamarwarsi@outlook.com>
2025-01-09 16:31:05 +0530

    create delete comments button with controller and routes

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -21,3 +21,24 @@ module.exports.create = async function(req, res){
         return res.status(500).send('Internal Server Error')
     }
 }
+
+module.exports.destroy = async function(req, res){
+    try {
+        let reqComment = await Comment.findById(req.params.id);
+        if (reqComment.user == req.user.id) {
+            let postID = reqComment.post;
+
+            await reqComment.deleteOne();
+
+            await Post.findByIdAndUpdate(postID, {$pull: {comments: req.params.id}});
+
+            return res.redirect(req.get('Referer') || '/')
+        }
+        else{
+            return res.redirect('/')
+        }
+    } catch (err) {
+        console.error("Error in comment deletion:", err);
+        return res.status(500).send("Internal Server Error");
+    }
+}
