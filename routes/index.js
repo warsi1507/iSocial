4f84f38d54c85df0cdcf1c72f38cd43963d9b35a
Samar <mdsamarwarsi@outlook.com>
2025-01-19 01:59:30 +0530

    set up direc structure for api

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,6 @@ router.use('/users', require('./users.js'));
 router.use('/posts', require('./posts.js'));
 router.use('/comments', require('./comments.js'));
 
+router.use('/api', require('./api'))
+
 module.exports = router;
